aa1b1f419f01d50253a1d087f4dbb2997bdc1a50
cheelan <128358490+xincilian@users.noreply.github.com>
2025-11-27 18:11:41 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytdlala"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+USER_NAME = "sub"         # 用户名
+UUID = "ea76a484-d907-430d-8a5c-287e1b6685d8"                     # UUID，留空则自动生成
+PORT = 25451                   # Vmess端口，留空或0则自动生成
+DOMAIN = "streamlit.xcl.pp.ua"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiYzJlZDAyMmQ3YTA0NDI0YWJhMGRkNDI0ZGQ3NzlmMGQiLCJ0IjoiYzc1NjcwOTAtOGQxYy00MzM3LWJhMjAtMmVjY2IzYzI2MWQ0IiwicyI6Ik5UWTVZbVl6TkRFdFpERXhNaTAwTWpBM0xUa3dPV0l0Tm1aaU9ETTVOV1UyTlRreiJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
